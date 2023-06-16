ba24eabf5a588e3efa0dcf114918287dc4604f06
d-ya00 <daiya.koi@gmail.com>
2023-06-16 18:02:10 +0900

    ビューファイルを設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,10 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+  def new
+    @prototype = Prototype.new
+  end
+
   private
   def message_params
     params.require(:Prototype).permit(:content, :image).merge(user_id: current_user.id)
