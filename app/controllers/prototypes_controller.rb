8ab3d5abda28f78f7df291cf444d57a3835f144b
d-ya00 <daiya.koi@gmail.com>
2023-06-16 16:59:32 +0900

    Active Storageの導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,8 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+  private
+  def message_params
+    params.require(:Prototype).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
