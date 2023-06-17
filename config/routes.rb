0b1963ae490ad18d50251e2dbe771cad2aea9d02
d-ya00 <daiya.koi@gmail.com>
2023-06-17 21:11:17 +0900

    プロトタイプ情報の投稿機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create]
+  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :users, only: [:show] 
 end
