026bc9702b6a1eacb6e8a5a6ab6745922dad41c1
d-ya00 <daiya.koi@gmail.com>
2023-06-18 03:05:11 +0900

    プロトタイプ情報の編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create, :show, :edit]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: [:show] 
   resources :comments, only: [:create]
 end
