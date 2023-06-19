47a33a2dd7251f67c7fd7de2ed7bbe51953e3667
d-ya00 <daiya.koi@gmail.com>
2023-06-20 00:39:58 +0900

    修正が完了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :move_to_index, only: [:edit]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @prototypes = Prototype.all
@@ -30,8 +30,16 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
-    @prototype.update(prototype_params)
-    redirect_to root_path
+
+    if params[:prototype].values.any?(&:blank?)
+      # データが空の場合は編集ページに留まる
+      render :edit
+    elsif @prototype.update(prototype_params)
+      redirect_to @prototype
+    else
+      # その他のエラーがある場合は編集ページに留まる
+      render :edit
+    end
   end
 
   def destroy
@@ -45,9 +53,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  
+
   def move_to_index
     unless user_signed_in? && current_user == Prototype.find(params[:id]).user
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
-  end
+  
+end
 end
